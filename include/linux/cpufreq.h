fa0b6f805c18471fae03ed1217feaa3cb8bb9d46
ayysir <dresadd09691@gmail.com>
2013-02-22 23:56:41 -0500

    Compile & New Governor: little changes to compile makefile & New ASSWAX Governor
    New Governor: ASSWAX (A work in progress of merging BrazilianWax and Smartass into Asswax; Originally Based on the interactive governor By Mike Chan (mike@android.com)
    which was adaptated to 2.6.29 kernel by Nadlabak (pavel@doshaska.net))
    
    	modified:   drivers/cpufreq/Kconfig
    	modified:   drivers/cpufreq/Makefile
    	new file:   drivers/cpufreq/cpufreq_asswax.c
    	modified:   include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -478,6 +478,9 @@ extern struct cpufreq_governor cpufreq_gov_gallumaufry;
 #elif defined(CONFIG_CPU_FREQ_DEFAULG_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lionheart)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ASSWAX)
+extern struct cpufreq_governor cpufreq_gov_asswax;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_asswax)
 #endif
 
 
