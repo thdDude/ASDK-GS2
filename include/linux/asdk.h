5324cfb97d433c4ff4ec19647ad4397d59e46b5d
ayysir <dresadd09691@gmail.com>
2013-04-16 12:31:51 -0400

    custom header

---
+++ include/linux/asdk.h
@@ -0,0 +1,27 @@
+/* include/linux/asdk.h
+ *
+ *  Copyright (c) 2013 Ayysir
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ * 
+*/
+
+#ifndef LINUX_ASDK_H
+#define LINUX_ASDK_H
+
+int ASDK_MIN_FREQ 384000
+int ASDK_MAX_FREQ 1512000
+
+#define FREQSTEPS 31
+
+#define MIN_VDD_SC		700000 /* uV */
+#define MAX_VDD_SC		1350000 /* uV */
+#define MAX_VDD_MEM		1350000 /* uV */
+#define MAX_VDD_DIG		1350000 /* uV */
+
+
+#endif
+
